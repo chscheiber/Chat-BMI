4b3bde8cb370c4848555230561e82bd30b47d882
Christoph Scheiber <christoph.scheiber@any2any.co>
2023-07-22 18:50:37 +1000

    set-up skeleton

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	// Your selected Skeleton theme:
+	import '@skeletonlabs/skeleton/themes/theme-rocket.css';
+
+	// This contains the bulk of Skeletons required styles:
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+
+	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
+	import '../app.postcss';
+</script>
+
+<div class="app-body">
+	<slot />
+</div>
+
+<style>
+	.app-body {
+		margin: 16px;
+	}
+</style>
---
+++ src/routes/miro/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<button type="button" class="btn variant-filled">Button</button>
