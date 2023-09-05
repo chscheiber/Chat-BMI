eecaf72e3fe06aa6e0723e8444db89b65bf73e16
Christoph Scheiber <christoph.scheiber@any2any.co>
2023-09-05 12:21:26 +0200

    improve prompt finder

--- src/lib/components/PromptFinder/PromptFinder.svelte
+++ src/lib/components/PromptFinder/PromptFinder.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
 	import { PromptFactory, type Prompt, type PromptData, type PromptTypeKey } from '$lib/models';
-	import Icon from '@iconify/svelte';
+	import { ProgressRadial } from '@skeletonlabs/skeleton';
+	import { load } from '../../../routes/miro/prompts/[prompt_type]/+page';
 	import RunConfigButtons from '../RunConfigButtons.svelte';
 
 	let prompts: Prompt[] = [];
 	let searchValue = '';
 
 	let timer: NodeJS.Timeout;
+	let loading = false;
 	const debounce = (e: any) => {
 		clearTimeout(timer);
-		if (e.target.value === '') {
+		if (e.target.value == '') {
 			searchValue = e.target.value;
 		} else {
 			timer = setTimeout(() => {
@@ -19,16 +21,25 @@
 	};
 
 	$: {
+		const queryPrompts: Prompt[] = [];
 		if (searchValue != '' && searchValue.length > 2) {
+			loading = true;
 			fetch(`/api/prompts?value=${searchValue}&limit=5`)
 				.then((res) => res.json())
 				.then((data: PromptData[]) => {
-					prompts = [];
 					for (const promptData of data) {
-						prompts.push(PromptFactory.createPrompt(promptData.type as PromptTypeKey, promptData));
+						queryPrompts.push(
+							PromptFactory.createPrompt(promptData.type as PromptTypeKey, promptData)
+						);
 					}
 				})
-				.catch((err) => console.error(err));
+				.catch((err) => {
+					console.error(err);
+				})
+				.finally(() => {
+					prompts = queryPrompts;
+					loading = false;
+				});
 		} else {
 			prompts = [];
 		}
@@ -60,6 +71,10 @@
 					<RunConfigButtons {prompt} />
 				</div>
 			{/each}
+		</div>
+	{:else if loading}
+		<div class="flex justify-center">
+			<ProgressRadial width={'w-10'} stroke={100} />
 		</div>
 	{:else}
 		<p class="text-gray-500">No prompts found<br />Type at least 3 characters to find prompts...</p>
